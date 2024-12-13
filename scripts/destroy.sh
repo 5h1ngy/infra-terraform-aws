fb7d63a20de6dc56bf4cdeb7de34de32d8376ed4
5h1ngy <sig.scarano@outlook.it>
2024-12-13 17:38:05 +0100

    feat: :sparkles: add directory checks for Terraform files in clean, deploy, and destroy scripts

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -1,3 +1,24 @@
-cd ..
+# Debug: Mostra la directory corrente e i file presenti
+pwd
+ls -l
 
+#!/bin/bash
+
+# Controlla se ci sono file Terraform nella directory corrente
+if ls *.tf > /dev/null 2>&1; then
+  echo "Terraform files found in the current directory."
+else
+  echo "No Terraform files in the current directory. Changing directory..."
+  cd ..
+  
+  # Controlla di nuovo nella directory superiore
+  if ls *.tf > /dev/null 2>&1; then
+    echo "Terraform files found in the parent directory."
+  else
+    echo "No Terraform files found in the parent directory. Exiting."
+    exit 1
+  fi
+fi
+
+# Esegui i comandi Terraform
 terraform destroy;
