36510b55ee29a7390eb6b7a43faeab2f54898f64
5h1ngy <sig.scarano@outlook.it>
2024-12-18 00:43:36 +0100

    fix(dns): :bug: data module not found

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,24 +1,24 @@
-data "aws_route53_zone" "avatar_maker_click" {
+data "aws_route53_zone" "domain_click" {
   name         = "5h1ngy.click"
   private_zone = false
 }
 
-# resource "aws_route53_zone" "avatar_maker_click" {
+# resource "aws_route53_zone" "domain_click" {
 #   name = "5h1ngy.click"
 # }
 
-resource "aws_route53_record" "avatar_maker_click" {
-  # zone_id = data.aws_route53_zone.avatar_maker_click.zone_id
-  zone_id = aws_route53_zone.avatar_maker_click.zone_id
+resource "aws_route53_record" "domain_click" {
+  # zone_id = aws_route53_zone.domain_click.zone_id
+  zone_id = data.aws_route53_zone.domain_click.zone_id
   name    = "5h1ngy.click" # Corretto
   type    = "A"
   ttl     = 300
   records = [aws_instance.frontend.public_ip]
 }
 
-resource "aws_route53_record" "www_avatar_maker_click" {
-  # zone_id = data.aws_route53_zone.avatar_maker_click.zone_id
-  zone_id = aws_route53_zone.avatar_maker_click.zone_id
+resource "aws_route53_record" "www_domain_click" {
+  # zone_id = aws_route53_zone.domain_click.zone_id
+  zone_id = data.aws_route53_zone.domain_click.zone_id
   name    = "www.5h1ngy.click"
   type    = "A"
   ttl     = 300
